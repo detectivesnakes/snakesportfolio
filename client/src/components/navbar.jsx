96a1438e5c7ac33e637fd285af0c8636ac1db4c6
detectivesnakes <157449001+detectivesnakes@users.noreply.github.com>
2024-05-20 18:20:51 -0700

    Good Times

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -20,36 +20,51 @@ const Navbar = () => {
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">
+                Home
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Link
+              </a>
+            </li>
             <li class="nav-item dropdown">
               <a
                 class="nav-link dropdown-toggle"
-                href="/#"
+                href="#"
                 role="button"
                 data-bs-toggle="dropdown"
                 aria-expanded="false"
               >
-                Projects
+                Dropdown
               </a>
               <ul class="dropdown-menu">
                 <li>
-                  <a class="dropdown-item" href="/#">
-                    Test1
+                  <a class="dropdown-item" href="#">
+                    Action
                   </a>
                 </li>
                 <li>
-                  <a class="dropdown-item" href="/#">
-                    Test2
+                  <a class="dropdown-item" href="#">
+                    Another action
                   </a>
                 </li>
-                {/*<li>
+                <li>
                   <hr class="dropdown-divider" />
                 </li>
                 <li>
-                  <a class="dropdown-item" href="/#">
+                  <a class="dropdown-item" href="#">
                     Something else here
                   </a>
-                </li>*/}
+                </li>
               </ul>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" aria-disabled="true">
+                Disabled
+              </a>
             </li>
           </ul>
           <form class="d-flex" role="search">
